b4c373d6983c0ea1d015cf8ffa6cb2732e224b76
dioooonis <81553110+dioooonis@users.noreply.github.com>
2023-02-03 23:33:57 +0100

    first commit

---
+++ src/test/java/com/example/demotoload/DemotoloadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demotoload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemotoloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
